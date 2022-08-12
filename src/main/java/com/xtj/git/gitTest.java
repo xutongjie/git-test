d7c2fab93c9a63b4e7604982dc8e6201ae3e9938
XTJ <XTJ@123.com>
2022-08-12 15:17:37 +0800

    third commit

--- src/main/java/com/xtj/git/gitTest.java
+++ src/main/java/com/xtj/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("你好啊hello git");
         System.out.println("你好啊hello git2");
+        System.out.println("你好啊hello git3");
     }
 }
