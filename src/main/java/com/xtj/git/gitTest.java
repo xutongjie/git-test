1d47303dd8625bc50029530f463917e0b87278c5
XTJ <XTJ@123.com>
2022-08-12 15:51:42 +0800

    hot-fix commit2

--- src/main/java/com/xtj/git/gitTest.java
+++ src/main/java/com/xtj/git/gitTest.java
@@ -5,5 +5,7 @@ public class gitTest {
         System.out.println("你好啊hello git");
         System.out.println("你好啊hello git2");
         System.out.println("你好啊hello git3");
+        System.out.println("你好啊hello git4");
+        System.out.println("hot-fix commit2");
     }
 }
